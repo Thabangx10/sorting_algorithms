cce8a764a5a9fbc1ef2da953d3cb6907da3b1961
Thabangx10 <Thabangx10@gmail.com>
2023-01-27 00:01:49 -0800

    1

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,43 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - sorts by the marker of the array
+ * @list: a listint type
+ *
+ * Return: void
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *temp = NULL, *mark = NULL;
+
+	if ((*list)->next == NULL || list == NULL || (*list) == NULL)
+		return;
+
+	/* back tracking from marker - editor */
+	temp = *list;
+
+	/* marker point in the array */
+	marker = *list;
+
+	while (marker != NULL)
+	{
+		marker = marker->next;
+		
+		while (temp->prev && (temp->n < temp->prev->n))
+		{
+			temp->prev->next = temp->next;
+			if (temp->next != NULL)
+				temp->next->prev = temp->prev;
+
+			temp->next = temp->prev;
+			temp->prev = temp->next->prev;
+			if (temp->prev != NULL)
+				temp->prev->next = temp;
+			else
+				*list = temp;
+			temp->next->prev = temp;
+			print_list(*list);
+		}
+		temp = marker;
+	}
+}
